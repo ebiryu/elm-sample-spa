a3b1ddcd3bda59ac5a637a560cca5fbaf659c2b2
ebiryu <ebiryu.zatta@gmail.com>
2017-11-12 18:20:22 +0100

    add horizontal scroll

--- src/Search/View.elm
+++ src/Search/View.elm
@@ -11,17 +11,23 @@ view : Model -> Html Msg
 view model =
     div [ class "bg-blue w-100 h-100 absolute top-0 left-0 fixed" ]
         [ i [ class "material-icons md-48 ml3 ml5-ns mt5 white pointer", onClick ToggleSearch ] [ text "clear" ]
-        , div [ class "mh-3 mh5-ns w-75" ]
+        , div
+            [ class "mh3 mh5-ns db overflow-x-auto nowrap"
+            ]
             [ searchFormView model
             , howManyPeopleView model
             ]
         ]
 
 
+inlineClass =
+    "w-75 h-75 dib v-top mh3 pa3 br3 bg-white-10 "
+
+
 searchFormView : Model -> Html Msg
 searchFormView model =
-    div [ class "w-100 inline-block" ]
-        [ div [ class "f3 pv2 white" ] [ text "検索" ]
+    div [ class (inlineClass ++ "") ]
+        [ div [ class "f3 mb2 white" ] [ text "検索" ]
         , input
             [ id "search-place"
             , type_ "search"
@@ -38,7 +44,7 @@ searchFormView model =
 searchResultList : ( Model.PlaceId, String ) -> Html Msg
 searchResultList ( id, string ) =
     li
-        [ class "b--white bb bw1 br2 ma1 ph2 pv3 white f3 hover-bg-white-20 pointer"
+        [ class "b--white bb bw1 br2 br--top ma1 ph2 pv3 white f3 hover-bg-white-20 pointer"
         , onClick (SelectCityId id)
         ]
         [ text string ]
@@ -46,4 +52,5 @@ searchResultList ( id, string ) =
 
 howManyPeopleView : Model -> Html Msg
 howManyPeopleView model =
-    div [] []
+    div [ class inlineClass ]
+        [ div [ class "f3 mb2 white" ] [ text "人数" ] ]
