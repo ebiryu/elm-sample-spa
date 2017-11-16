e6c9ddc68b2979a8521dbdb3a23c2feab95a954b
ebiryu <ebiryu.zatta@gmail.com>
2017-11-17 00:42:32 +0100

    add Date.Extra

--- src/Search/DatePicker.elm
+++ src/Search/DatePicker.elm
@@ -1,12 +1,17 @@
 module Search.DatePicker exposing (..)
 
 import Date
+import Date.Extra.Config.Config_ja_jp exposing (config)
+import Date.Extra.Core exposing (daysInMonth, intToMonth, isoDayOfWeek, toFirstOfMonth)
+import Date.Extra.Duration as Duration
+import Date.Extra.Field as Field
+import Date.Extra.Format as DateFormat
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
 view model =
-    div [ class "w-100 h-75 center relative" ]
+    div [ class "w-100 h-75 center" ]
         [ div [ class "tc" ]
             [ div [ class "dib w5 f6 f5-l white pa2 ba br2 b--white ma1 hover-bg-white-20 pointer" ]
                 [ text "チェックイン: "
@@ -16,7 +21,29 @@ view model =
                 [ text "チェックアウト: "
                 , text model.dateCheckOut
                 ]
+            , datePicker model
             ]
-        , div [ class "absolute absolute--fill ma-auto mw6 h5 bg-white-10" ]
-            [ text <| toString <| Date.fromString model.dateNow ]
         ]
+
+
+datePicker model =
+    div [ class "absolute absolute--fill bg-black-10 z2" ]
+        [ div [ class "absolute absolute--fill ma-auto mw6 h5 bg-white-10" ]
+            [ header
+            , picker
+            ]
+        ]
+
+
+header =
+    div [ class "db bg-light-blue w-100 h-25" ]
+        [ text <|
+            Result.withDefault "Failed to get a date." <|
+                Result.map
+                    (DateFormat.format config config.format.dateTime)
+                    (Date.fromString "2015-06-01 12:45:14.211Z")
+        ]
+
+
+picker =
+    div [ class "db bg-white w-100 h-75" ] []
