1fd3d8db60622badc4288ae1af4408186318386a
ebiryu <ebiryu.zatta@gmail.com>
2017-11-17 23:58:35 +0100

    conplete calendar

--- src/Search/DatePicker.elm
+++ src/Search/DatePicker.elm
@@ -57,4 +57,60 @@ picker model =
             , button [ class "pointer outline-0", onClick (DatePickerMsg NextMonth check) ]
                 [ i [ class "material-icons md-24 near-black" ] [ text "navigate_next" ] ]
             ]
+        , weekDays
+        , monthDays model
         ]
+
+
+weekDays : Html Msg.Msg
+weekDays =
+    let
+        days =
+            List.map (\day -> span [ style [ ( "width", "14%" ) ] ] [ text day ]) [ "日", "月", "火", "水", "木", "金", "土" ]
+    in
+    div [ class "flex justify-between tc" ] days
+
+
+monthDays : Model -> Html Msg.Msg
+monthDays model =
+    let
+        daysCount =
+            daysInMonth (Date.year model.date) (Date.month model.date)
+
+        leftPadding =
+            model.date
+                |> toFirstOfMonth
+                |> Date.dayOfWeek
+                |> isoDayOfWeek
+
+        rightPadding =
+            42 - leftPadding - daysCount
+
+        weeks =
+            chunks 7 (List.repeat leftPadding 0 ++ List.range 1 daysCount ++ List.repeat rightPadding 0)
+
+        rows =
+            List.map (\week -> weekRow (Date.day model.date) week) weeks
+    in
+    div [] rows
+
+
+chunks : Int -> List Int -> List (List Int)
+chunks a xs =
+    if List.length xs > a then
+        List.take a xs :: chunks a (List.drop a xs)
+    else
+        [ xs ]
+
+
+weekRow : Int -> List Int -> Html Msg.Msg
+weekRow currentDay days =
+    div [ class "flex justify-between tc" ] (List.map (dayCell currentDay) days)
+
+
+dayCell : Int -> Int -> Html Msg.Msg
+dayCell currentDay day =
+    if day > 0 then
+        div [ style [ ( "width", "14%" ) ] ] [ div [] [ text (toString day) ] ]
+    else
+        div [ style [ ( "width", "14%" ) ] ] [ div [] [ text "" ] ]
